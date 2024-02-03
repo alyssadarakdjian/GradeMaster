f1cf947db97efe312a253c4e51657b661af04246
vmgriest <vmgriest@oakland.edu>
2024-02-03 15:46:36 -0500

    Additions To TeacherGradeBookScene

--- src/application/TeacherGradeBookSceneController.java
+++ src/application/TeacherGradeBookSceneController.java
@@ -21,9 +21,6 @@ public class TeacherGradeBookSceneController implements Initializable{
 	private TableView<AssData> TableView1;
 	
 	@FXML
-	private TableView<AssData> TableView2;
-	
-	@FXML
 	private TableColumn<AssData,String> Ass;
 	
 	@FXML
@@ -33,13 +30,16 @@ public class TeacherGradeBookSceneController implements Initializable{
 	private TableColumn<AssData,Integer> Weight;
 	
 	@FXML
-	private TableColumn<AssData,String> Ass2;
+	private TableView<GradeData> TableView2;
 	
 	@FXML
-	private TableColumn<AssData,String> Type2;
+	private TableColumn<GradeData, String> Ass2;
 	
 	@FXML
-	private TableColumn<AssData,Integer> Weight2;
+	private TableColumn<GradeData, Integer> Grade;
+	
+	@FXML
+	private TableColumn<GradeData, String> Student;
 	
 	@FXML
 	private TextField AssTextField;
@@ -49,6 +49,15 @@ public class TeacherGradeBookSceneController implements Initializable{
 	
 	@FXML
 	private TextField WeightTextField;
+
+	@FXML
+	private TextField AssTextField2;
+	
+	@FXML
+	private TextField StudentTextField;
+	
+	@FXML
+	private TextField GradeTextField;
 	
 	@FXML
 	private Button CreateAssButton;
@@ -67,11 +76,23 @@ public class TeacherGradeBookSceneController implements Initializable{
 		Ass.setCellValueFactory(new PropertyValueFactory<AssData,String>("Ass"));
 		Type.setCellValueFactory(new PropertyValueFactory<AssData,String>("Type"));
 		Weight.setCellValueFactory(new PropertyValueFactory<AssData,Integer>("Weight"));
+		
+		Ass2.setCellValueFactory(new PropertyValueFactory<GradeData,String>("Student"));
+		Student.setCellValueFactory(new PropertyValueFactory<GradeData,String>("Student"));
+		Grade.setCellValueFactory(new PropertyValueFactory<GradeData,Integer>("Grade"));
 	}
 	
 	@FXML
 	void CreateAss(ActionEvent event) {
+		String AssText = AssTextField.getText().trim();
+	    String TypeText = TypeTextField.getText().trim();
+	    String WeightText= WeightTextField.getText().trim();
 	    
+	    if (AssText.isEmpty() || TypeText.isEmpty()||WeightText.isEmpty()) {
+	        showAlert("Error", "Empty Fields", "Please enter Assignment Name, Student Name, and Grade.");
+	        return;
+	    }
+	    try {
 	    AssData assdata = new AssData(AssTextField.getText(),
 	            TypeTextField.getText(),
 	            Integer.parseInt(WeightTextField.getText()));
@@ -79,6 +100,11 @@ public class TeacherGradeBookSceneController implements Initializable{
 	    ObservableList<AssData> assdatas1 = TableView1.getItems();
 	    assdatas1.add(assdata);
 	    TableView1.setItems(assdatas1);
+	    } catch (NumberFormatException e) {
+	        showAlert("Error", "Invalid Number", "Please enter a valid Weight Number.");
+	    }
+	    
+	    
 	}
 
 	
@@ -96,6 +122,49 @@ public class TeacherGradeBookSceneController implements Initializable{
 
 	    TableView1.getItems().remove(selectedID);
 	}
+	
+	@FXML
+	void InputGrade(ActionEvent event) {
+		 	String AssText = AssTextField2.getText().trim();
+		    String StudentText = StudentTextField.getText().trim();
+		    String GradeText= GradeTextField.getText().trim();
+		    if (AssText.isEmpty() || StudentText.isEmpty()||GradeText.isEmpty()) {
+		        showAlert("Error", "Empty Fields", "Please enter Assignment Name, Student Name, and Grade.");
+		        return;
+		    }
+		    try {
+	    	GradeData gradedata = new GradeData(AssTextField2.getText(),
+		            StudentTextField.getText(),
+		            Integer.parseInt(GradeTextField.getText()));
+	    	ObservableList<GradeData> gradedatas1 = TableView2.getItems();
+		    gradedatas1.add(gradedata);
+		    TableView2.setItems(gradedatas1);
+		    } catch (NumberFormatException e) {
+		        showAlert("Error", "Invalid Number", "Please enter a valid Grade Number.");
+		    }
+		   
+
+		   
+	}
+
+
+	@FXML
+	void RemoveGrade(ActionEvent event) {
+	    if (TableView2.getItems().isEmpty()) {
+	        showAlert("Error", "TableView2 is empty", "Please add assignments with grades to Table before removing.");
+	        return;
+	    }
+
+	    int selectedID = TableView2.getSelectionModel().getSelectedIndex();
+	    if (selectedID == -1) {
+	        showAlert("Error", "No assignment selected", "Please select an assignment from the Table before removing grades.");
+	        return;
+	    }
+
+	    TableView2.getItems().remove(selectedID);
+	}
+
+
 
 	private void showAlert(String title, String header, String content) {
 	    Alert alert = new Alert(AlertType.ERROR);
