9a495295b20cf61579bb5ac2b85becefa240eb99
escobaradelaida <adelaidaescobar@oakland.edu>
2024-03-16 16:50:03 -0400

    Added colors to button check if git worked

--- GradeMaster/src/application/DatabaseConnection.java
+++ GradeMaster/src/application/DatabaseConnection.java
@@ -7,10 +7,14 @@ public class DatabaseConnection {
 	public Connection databaseLink;
 	
 	public Connection getConnection() {
-		String databaseName = "loginbase";
-		String databaseUser = "root";
-		String databasePassword = "gradedatabase";
-		String url = "jdbc:mysql://localhost/" + databaseName;
+		// was "loginbase"
+		String url = "grademaster-mysql.mysql.database.azure.com";
+		// was "root"
+		String databaseUser = "GradeMaster";
+		// was "gradedatabase"
+		String databasePassword = "Justice_League";
+		// was "jdbc:mysql://localhost/" + databaseName
+		//String url = "jdbc:mysql://localhost/" + databaseName;
 		
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
