c69147bdb277273ea87cd73a54a6be0bdf460047
Lebstan <156549948+Lebstan@users.noreply.github.com>
2024-03-22 14:29:14 -0400

    Update SwitchSceneController.java

--- SwitchSceneController.java
+++ SwitchSceneController.java
@@ -47,6 +47,14 @@ public class SwitchSceneController {
 		stage.setScene(scene);
 		stage.show();
 	}
+
+	public void switchToGraphScene(ActionEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("Graphs.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
 	
 	public void switchToTempScene(ActionEvent event) throws IOException {
 		root = FXMLLoader.load(getClass().getResource("MenuScene.fxml"));
