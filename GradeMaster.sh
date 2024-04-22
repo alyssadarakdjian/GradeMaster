990b0cd0042f221c2322d4c1a9ffe0e1fee56f85
Lebstan <156549948+Lebstan@users.noreply.github.com>
2024-04-22 11:44:28 -0400

    Update GradeMaster.sh

--- GradeMaster.sh
+++ GradeMaster.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java --module-path /Users/NG/Downloads/javafx-sdk-21.0.2/lib --add-modules javafx.controls,javafx.fxml -jar GradeMaster.jar
+java --module-path /Users/.../javafx-sdk-21.0.2/lib --add-modules javafx.controls,javafx.fxml -jar GradeMaster.jar
